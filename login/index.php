0abe48baa9490b593b1d921f488f08261d8db072
mmarbonillo <mbonillo95@gmail.com>
2018-03-06 13:02:34 +0100

    por fin sale el login TT.TT

--- login/index.php
+++ login/index.php
@@ -1,9 +1,3 @@
-<?php
-// including the database connection file
-include_once("../config.php");
-?>
-
-
 <!doctype html>
 <html lang="en">
   <head>
@@ -23,13 +17,13 @@ include_once("../config.php");
   </head>
 
   <body class="text-center">
-    <form class="form-signin" method="post">
+    <form class="form-signin" method="get">
       <img class="mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
       <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
       <label for="inputEmail" class="sr-only">Email address</label>
-      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+      <input type="email" name="inputEmail" class="form-control" placeholder="Email address" required autofocus>
       <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+      <input type="password" name="inputPassword" class="form-control" placeholder="Password" required>
       <div class="checkbox mb-3">
         <label>
           <input type="checkbox" value="remember-me"> Remember me
@@ -40,17 +34,76 @@ include_once("../config.php");
     </form>
 
 <?php
-$mail = mysql_query($mysqli, "select email from usuarios");
-$passwd = mysql_query($mysqli, "select password from usuarios");
-$inputEmail = $_POST[inputEmail];
-$inputPasswd = $_POST[inputPassword];
+include("../config.php");
+
+/*
+echo "mailpost:";
+print_r($mailpost);
+echo "</br>";
+echo "passwdpost:";
+print_r($passwdpost);
+echo "</br>";
 
 
-$usuario_valido = mysql_query($mysqli, "select id from usuarios where email = '$inputEmail' and password = '$inputPasswd'");
+$mail = mysqli_query($mysqli, "SELECT 'password' FROM usuario WHERE email = '$mailpost'");
+$passwd = mysqli_query($mysqli, "SELECT 'email' FROM usuario WHERE password = '$passwdpost'");
 
-if($usuario_valido == true) {
-    echo "bien";
+echo "Email:";
+print_r($mail);
+echo "</br>";
+echo "Contraseña:";
+print_r($passwd);
+echo "</br>";
+
+$consulta = "SELECT * FROM usuario WHERE email = '$mailpost' AND password = '$passwdpost'";
+$usuario_valido = mysqli_query($mysqli, $consulta);
+$mail = mysqli_query($mysqli, "SELECT 'email' FROM usuario WHERE password = '$passwdpost'");
+$passwd = mysqli_query($mysqli, "SELECT 'password' FROM usuario WHERE email = '$mailpost'");
+print_r($mail);
+
+if($mail == $mailpost) {
+  echo "usuario correcto";
+  echo "</br>";
+  if ($passwd == $passwdpost) {
+    echo "contraseña correcta";
+    echo "por fin";
+  }
+}else {echo "poh va a seh que no chula";}
+
+
+
+if($mysqli) {
+  if($mailpost == $mail && $passwdpost == $passwd) {
+    echo "Bien";
+  } else {echo "no existe";}
+} else {echo "Conexión fallida";}
+
+print_r($usuario_valido);
+*/
+
+$mailpost = $_GET["inputEmail"];
+$passwdpost = md5($_GET["inputPassword"]);
+
+$consulta = mysqli_query ($mysqli, "SELECT email, password FROM usuario WHERE (email = '$mailpost') AND (password = '$passwdpost')");
+echo '</br>';echo '</br>';
+#print_r($consulta);
+echo '</br>';echo '</br>';
+
+while($res = mysqli_fetch_array($consulta)) {
+  echo "<tr>";
+  echo "<h4>".$res['email']."</h4>";
+  echo "<h4>".$res['password']."</h4>";
 }
+
+
+if($res['email'] = $mailpost) {
+  echo "el email existe";
+  echo '</br>';
+  if($res['password'] = $passwdpost) {
+    echo "la contraseña existe";
+  } else {echo "La contraseña no existe";}
+} else {echo "El usuario no existe";}
+
 
 ?>
   </body>
